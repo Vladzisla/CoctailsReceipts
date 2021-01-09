ede68dcea37978c9c3ee7378c22fbda3ed2a8196
Vlad <vladis192@gmail.com>
2021-01-09 05:43:11 +0300

    final

--- scripts/main.js
+++ scripts/main.js
@@ -29,18 +29,22 @@ const infoBlock = document.getElementById('info')
 
 
 addReceipt.onclick = function(){
-    const name = prompt('Введите название коктейля'),
-        isAlco = confirm('Алкогольный ли напиток?'),
-        ingredients = prompt('Введите нгредиенты и их количество'),
-        receipt = prompt('Введите рецепт коктейля')
+    const name = prompt('Введите название коктейля')
+    if(!name) return
+    const isAlco = confirm('Алкогольный ли напиток?')
+    const ingredients = prompt('Введите ингредиенты и их количество')
+    if(ingredients == null) return
+    const receipt = prompt('Введите рецепт коктейля')
+    if(receipt == null) return
 
     cocktailsStorage.addValue(name, {isAlco, ingredients, receipt})
 }
 getReceipt.onclick = function(){
     const name = prompt('Введите название коктейля')
+    if(!name) return
     if(cocktailsStorage.getValue(name)){
         infoBlock.innerText = `Коктейль ${name} (Алкогольный: ${cocktailsStorage.getValue(name).isAlco ? 'да': 'нет'})
-        Необходимые игредиенты:
+        Необходимые ингредиенты:
         ${cocktailsStorage.getValue(name).ingredients}
         Рецепт приготовления:
         ${cocktailsStorage.getValue(name).receipt}`
@@ -48,15 +52,36 @@ getReceipt.onclick = function(){
     else {
         infoBlock.innerText = 'Такого коктейля не существует'
     }
-
+}
+delReceipt.onclick = function(){
+    const name = prompt('Введите название коктейля')
+    if(!name) return
+    infoBlock.innerText = cocktailsStorage.deleteValue(name) ? 'Напиток удален' : 'Такого напитка не существовало'
+}
+getCocktails.onclick = () => {
+    infoBlock.innerText = null
+    cocktailsStorage.getKeys().forEach((elem) => {
+        infoBlock.append(document.createElement("span").innerText = elem)
+        infoBlock.append(document.createElement('br'))
+    })
 }
 
-
-
-
-
-
-
-cocktailsStorage.addValue('a',{isAlco:true, ingredients:'a,b,v,c,x,d', receipt:'sfdgsdfsd'})
-cocktailsStorage.addValue('b',{isAlco:true, ingredients:'gsdgsfdgd', receipt:'sffdgfdgdgd342'})
-// console.log(cocktailsStorage.deleteValue('a'))
+cocktailsStorage.addValue('Маргарита',{isAlco:true,
+    ingredients:'Водка Finlandia 50мл\n' +
+        'Кофейный ликер 25мл\n' +
+        'Лед в кубиках 120 г',
+    receipt:'Наполни стакан кубиками льда доверху, затем налей кофейный ликер 25 мл, водку 50 мл и размешай коктейльной ложкой.'})
+cocktailsStorage.addValue('Пеликан',{isAlco:false,
+    ingredients:'Гренадин Monin 10мл\n' +
+        'Клубничный сироп Monin 10мл\n' +
+        'Персиковый сок 150мл\n' +
+        'Лимонный сок 15мл\n' +
+        'Банан 110г\n' +
+        'Клубника 50г\n' +
+        'Дробленый лед 60г',
+    receipt:'Положи в блендер очищенную и нарезанную половинку банана и клубнику 2 ягоды. Налей лимонный сок 15 мл, гренадин 10 мл, клубничный сироп 10 мл и персиковый сок 150 мл. Добавь в блендер совок дробленого льда и взбей. Перелей в хайбол. Укрась кружком банана и половинкой клубники на коктейльной шпажке.'})
+cocktailsStorage.addValue('Пиранья',{isAlco:true,
+    ingredients:'Водка Finlandia 37мл\n' +
+        'Шоколадный ликер 25мл\n' +
+        'Кола 25мл',
+    receipt:'Влейте спиртное в низкий стакан, заполненный большим количеством колотого льда. Хорошо размешайте. Затем добавьте колу.'})
